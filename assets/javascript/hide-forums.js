c2c5399d450b2a9ce0fb9707c6a0b9449da677fb
Łukasz Gurdek <git@ukasiu.pl>
2015-06-17 17:23:38 +0200

    Initial version

---
+++ assets/javascript/hide-forums.js
@@ -0,0 +1,35 @@
+// https://forum.iiet.pl/styles/prosilver/template/hide-forums.js // credits unknown
+var hideForums_done = 0;
+var hideForums = function() {
+  if(hideForums_done == 1) return;
+  hideForums_done = 1;
+  var re = /(\d+)/;
+  var h3 = document.querySelector('#page-body h3');
+  var els = document.querySelectorAll('#page-body div.forabg div.inner ul.topiclist li.header');
+
+  for(var i = 0; i < els.length; i++ ) {
+    var f = function(el) {
+      var nr = re.exec(el.querySelector('dl > dt > a').getAttribute('href'))[0];
+      var tabelka = el.parentNode.parentNode.querySelector('.forums');
+  
+      if(localStorage.getItem('f'+nr) == 1) {
+        var tmp = tabelka.parentNode.parentNode;
+        var parent = tmp.parentNode;
+        parent.removeChild(tmp);
+        parent.insertBefore(tmp,h3);    
+        tabelka.style.display = 'none';
+      }
+      el.addEventListener('click', function(ev) {
+            if(tabelka.style.display != 'none') {
+              tabelka.style.display = 'none';
+              localStorage.setItem('f'+nr, 1);
+            } else {
+              tabelka.style.display = 'block';
+              localStorage.setItem('f'+nr, 0);
+            }
+          }, false);
+    };
+    f(els[i]);
+  }
+};
+document.addEventListener('DOMContentLoaded',hideForums,false);
---
+++ event/listener.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace iiet\hide_forums\event;
+
+/**
+* @ignore
+*/
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+* Event listener
+*/
+class listener implements EventSubscriberInterface
+{
+  /**
+  * Constructor
+  *
+  * @param \phpbb\controller\helper    $helper        Controller helper object
+  */
+  public function __construct()
+  {
+  }
+
+  static public function getSubscribedEvents()
+  {
+    return array(
+      'core.page_header_after'  => 'page_header_after'
+    );
+  }
+
+  public function page_header_after($event)
+  {
+    $this->template->assign_vars(array(
+      'T_HIDE_FORUMS_ASSETS_PATH' => './ext/iiet/hide_forums/assets'
+    ));
+  }
+
+}
